64bd197162aeb473f2677faae72fabc45b911940
AuthurExcalbern <authurexcalbern@gmail.com>
2018-05-09 21:57:58 +0800

    完成实验四

---
+++ 实验4/4_6.cpp
@@ -0,0 +1,185 @@
+#include<iostream>
+#include<list>
+#include<map>
+#include<string>
+#include<iterator>
+#include<cstdlib>
+#include<cctype>
+#include<algorithm>
+#include<stack>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::map;
+using std::list;
+using std::stack;
+
+typedef list<string> Rule;
+typedef list<Rule> Rule_collection;
+typedef map<string, Rule_collection> Grammar;
+
+bool space(char c);
+bool not_sapce(char c);
+list<string> split(const string& str);
+
+//从一个特定的输入流读入一个文法
+Grammar read_grammar(std::istream& in);
+
+//生成语句
+list<string> gen_sentence(const Grammar& g);
+
+//判断是否为语法
+bool bracketed(const string& s);
+
+//返回 [0, n) 中的一个随机整数
+int nrand(int n);
+
+//得到语句
+void 
+gen_aux(const Grammar& g, const string& word, list<string>& ret);
+
+int main(void)
+{
+    //生成语句
+    list<string> sentence = gen_sentence(read_grammar(cin));
+    
+    //输出第一个单词（如果存在
+    list<string>::const_iterator iter = sentence.begin();
+    if(!sentence.empty())
+    {
+        cout << *iter;
+        ++iter;
+    }
+    
+    //输出其余单词，每一个单词前都有一个空格
+    while(iter != sentence.end())
+    {
+        cout << " " << *iter;
+        ++iter;
+    }
+    
+    cout << endl;
+    
+    return 0;
+}
+
+bool space(char c)
+{
+    return isspace(c);
+}
+
+bool not_sapce(char c)
+{
+    return !isspace(c);
+}
+
+list<string> split(const string& str)
+{
+    typedef string::const_iterator iter;
+
+    list<string> ret;
+    iter i = str.begin();
+    while(i != str.end())
+    {
+        i = std::find_if(i, str.end(), not_sapce);
+        iter j = std::find_if(i, str.end(), space);
+
+        if(i != str.end())
+            ret.push_back(string(i, j));
+
+        i = j;
+    }
+
+    return ret;
+}
+
+//从一个特定的输入流读入一个文法
+Grammar read_grammar(std::istream& in)
+{
+    Grammar ret;
+    string line;
+    
+    //读输入
+    while(getline(in, line))
+    {
+        //将输入分割成单词
+        list<string> entry = split(line);
+        
+        if(!entry.empty())
+            ret[entry.front()].push_back(Rule(++entry.begin(), entry.end()));
+    }
+    
+    return ret;
+}
+
+//生成语句
+list<string> gen_sentence(const Grammar& g)
+{
+    list<string> ret;
+    stack<Rule> stack_rule;
+    //gen_aux(g, "<sentence>", ret);
+    
+    string word = "<sentence>";
+    Grammar::const_iterator first_iter = g.find(word);
+    const Rule_collection first_c = first_iter->second;
+    const Rule first_r = *(first_c.begin());
+    
+    stack_rule.push(first_r);
+    
+    while(!stack_rule.empty())
+    {
+        const Rule now_rule = stack_rule.top();
+        stack_rule.pop();
+        
+        for(Rule::const_iterator i = now_rule.begin(); i != now_rule.end(); i++)
+        {
+            if(!bracketed(*i))
+            {
+                ret.push_back(*i);
+            }
+            else
+            {
+                Grammar::const_iterator iter = g.find(*i);
+                if(iter == g.end())
+                    cout << "empty rule." << endl;//throw logic_error();
+                
+                //获取可能的规则集合
+                const Rule_collection& c = iter->second;
+                
+                //从规则集合中随机选择一条规则
+                //const Rule& r = c[nrand(c.size())];
+                int n = nrand(c.size());
+                Rule_collection::const_iterator c_iter = c.begin();
+                while(n--) c_iter++;
+                const Rule r = *c_iter;
+                
+                stack_rule.push(r);
+            }
+        }
+    }
+    
+    return ret;
+}
+
+//判断是否为语法
+bool bracketed(const string& s)
+{
+    return s.size() > 1 && s[0] == '<' && s[s.size()-1] == '>';
+}
+
+//返回 [0, n) 中的一个随机整数
+int nrand(int n)
+{
+    if(n <= 0 || n > RAND_MAX)
+        cout << "超过随机范围。" << endl;//throw domain_error("");
+    
+    
+    const int bucket_size = RAND_MAX / n;
+    
+    int r;
+    do r = rand() / bucket_size;
+    while(r >= n);
+    
+    return r;
+}
